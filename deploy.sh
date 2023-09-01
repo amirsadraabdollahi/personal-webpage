bf1e7ddf33957ea55a36f5cb5d1cceafee15942a
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-09-01 19:13:35 +0330

    add ssl certificate

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 docker build . -t mynginx;
-docker run --name mynginx --mount type=bind,source=$PWD/,target=/usr/share/nginx/html --mount type=bind,source=$PWD/nginx.conf,target=/etc/nginx/nginx.conf -d -p 8080:80 mynginx;
+docker run --name mynginx --mount type=bind,source=$PWD/,target=/usr/share/nginx/html --mount type=bind,source=$PWD/nginx.conf,target=/etc/nginx/nginx.conf -d -p 80:80 -p 443:443 mynginx;
+
